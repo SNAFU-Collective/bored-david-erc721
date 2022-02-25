0414908e03da1c11d37295fb3acd29e0c7fa549e
Shrikanta Mazumder <shrikanta@genuino.world>
2022-02-25 20:47:40 +0600

    remove log

--- migrations/1_bored_david.js
+++ migrations/1_bored_david.js
@@ -1,11 +1,5 @@
 require("dotenv").config();
 const BoredDavid = artifacts.require("BoredDavid.sol");
-console.log(
-  process.env.NAME,
-  process.env.SYMBOL,
-  process.env.BASE_URL,
-  process.env.NOT_REVEAL_URL
-);
 
 module.exports = function (deployer) {
   deployer.deploy(
