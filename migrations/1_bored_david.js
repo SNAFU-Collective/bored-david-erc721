a6422f9ebc1729ab9108b681c62945d7d107dad4
Dan <rusnac14@gmail.com>
2022-05-30 19:58:29 +0200

    Updated ipfs

--- migrations/1_bored_david.js
+++ migrations/1_bored_david.js
@@ -4,7 +4,7 @@ const chainParameters = {
   "ethereum": {
       name: "Bored David",
       symbol: "DAVID",//non sono sicuro del symbol
-      not_reveal_url: "https://ipfs.infura.io/ipfs/QmcZy2F9jbZVitUXGxapLojKi8NdirBNEDjjfi7UZQ8nTn",
+      not_reveal_url: "https://ipfs.infura.io/ipfs/QmeA68Y53B2kVps5BR3cZzS3WTJSPV2uyF5Uxc8zSZM5zA",
       rare_cost: ether('0.15'),
       common_cost: ether('0.03'),
       max_supply: 2166,
@@ -16,7 +16,7 @@ const chainParameters = {
   "gnosis": {
     name: "Bored David on Gnosis",
     symbol: "DAVID",
-    not_reveal_url: "https://ipfs.infura.io/ipfs/QmcZy2F9jbZVitUXGxapLojKi8NdirBNEDjjfi7UZQ8nTn",
+    not_reveal_url: "https://ipfs.infura.io/ipfs/QmRgdewi4YsqC7dUnoyNz1QnHdQ5hMisPh66P4kMLvS63D",
     rare_cost: ether('300'),
     common_cost: ether('50'),
     max_supply: 2000,
@@ -28,7 +28,7 @@ const chainParameters = {
   "bsc": {
     name: "Bored David on BSC",
     symbol: "DAVID",
-    not_reveal_url: "https://ipfs.infura.io/ipfs/QmcZy2F9jbZVitUXGxapLojKi8NdirBNEDjjfi7UZQ8nTn",
+    not_reveal_url: "https://ipfs.infura.io/ipfs/QmNS9DiFDfPGNu8zfrxq61ZZoAgap3B6GWM23aBHnLTXfn",
     rare_cost: ether('1'),
     common_cost: ether('0.16'),
     max_supply: 1000,
@@ -40,7 +40,7 @@ const chainParameters = {
   "avalanche": {
     name: "Bored David on Avalanche",
     symbol: "DAVID",
-    not_reveal_url: "https://ipfs.infura.io/ipfs/QmcZy2F9jbZVitUXGxapLojKi8NdirBNEDjjfi7UZQ8nTn",
+    not_reveal_url: "https://ipfs.infura.io/ipfs/Qmbm3JzrkivKGvAghsmMbJV6F1wZCkY722uoiZoeS3gEWX",
     rare_cost: ether('11'),
     common_cost: ether('2'),
     max_supply: 500,
@@ -52,7 +52,7 @@ const chainParameters = {
   "polygon": {
     name: "Bored David on Polygon",
     symbol: "DAVID",
-    not_reveal_url: "https://ipfs.infura.io/ipfs/QmcZy2F9jbZVitUXGxapLojKi8NdirBNEDjjfi7UZQ8nTn",
+    not_reveal_url: "https://ipfs.infura.io/ipfs/QmWpH3ZQ91BfXjQMpWqdf4GNUXRwbTXzrftSmqDZmmNWjo",
     rare_cost: ether('480'),
     common_cost: ether('80'),
     max_supply: 500,
@@ -64,7 +64,7 @@ const chainParameters = {
   "aurora": {
     name: "Bored David on Aurora",
     symbol: "DAVID",
-    not_reveal_url: "https://ipfs.infura.io/ipfs/QmcZy2F9jbZVitUXGxapLojKi8NdirBNEDjjfi7UZQ8nTn",
+    not_reveal_url: "https://ipfs.infura.io/ipfs/QmP59nQCKiFHL1yydEJGUxUpCveiPKYP5LyuEVoSq9YtFE",
     rare_cost: ether('100'),
     common_cost: ether('15'),
     max_supply: 500,
